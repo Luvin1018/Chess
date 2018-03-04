2dde54ab966cd68745e857dccbb0496ead99fc28
VioletYuMengjie <36877052+VioletYuMengjie@users.noreply.github.com>
2018-03-04 14:26:25 +0800

    Add files via upload

---
+++ King.java
@@ -0,0 +1,25 @@
+public class King extends Chess
+{
+   private int x;
+   private int y;
+   private String color;
+   private King(String color)
+   {
+       this.color= color;
+       Chess.num+=1;
+   }
+   public String toString()
+   {
+        return color+"Ki    ";
+   }
+   public King creat(String color)
+   {
+        if(checkNum())
+        {
+            return new King(color);
+        }
+        else{
+            throw new Error("more than 32 ");
+        }
+   }
+}
