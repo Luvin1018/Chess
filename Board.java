3fab65ebeafd6e92c6e83bd2ea7a0d67af27d53b
deillusion <36877070+deillusion@users.noreply.github.com>
2018-03-05 11:32:52 +0800

    Add files via upload

--- Board.java
+++ Board.java
@@ -1,39 +1,30 @@
-
-/**
- * 在这里给出对类 Board 的描述。
- * 
- * @作者（你的名字）
- * @版本（一个版本号或者一个日期）
- */
 public class Board
 {
     private Chess[][] table=new Chess[8][8];
-    private King wk= new King("W");
-    private King bk= new King("B");
     public Board(){       
         for(int i=0;i<8;i++){
-            table[1][i]=new Pawn("W",i);
-            table[6][i]=new Pawn("B",i);
+            table[1][i]=new Pawn("W");
+            table[6][i]=new Pawn("B");
             for(int j=2;j<6;j++){
                 table[j][i]=new Chess();
             }
         }
-        table[0][0]=new Rook("W",0);
-        table[0][7]=new Rook("W",7);
-        table[7][0]=new Rook("B",0);
-        table[7][7]=new Rook("B",7);
-        table[0][1]=new Knight("W",1);
-        table[0][6]=new Knight("W",6);
-        table[7][1]=new Knight("B",1);
-        table[7][6]=new Knight("B",6);
-        table[0][2]=new Bishop("W",2);
-        table[0][5]=new Bishop("W",5);
-        table[7][2]=new Bishop("B",2);
-        table[7][5]=new Bishop("B",5);
+        table[0][0]=new Rook("W");
+        table[0][7]=new Rook("W");
+        table[7][0]=new Rook("B");
+        table[7][7]=new Rook("B");
+        table[0][1]=new Knight("W");
+        table[0][6]=new Knight("W");
+        table[7][1]=new Knight("B");
+        table[7][6]=new Knight("B");
+        table[0][2]=new Bishop("W");
+        table[0][5]=new Bishop("W");
+        table[7][2]=new Bishop("B");
+        table[7][5]=new Bishop("B");
         table[0][3]=new Queen("W");
         table[7][3]=new Queen("B");
-        table[0][4]=wk;
-        table[7][4]=bk;
+        table[0][4]=new King("W");
+        table[7][4]=new King("B");
     }
     public void printOut(){
         for(int i=0;i<8;i++){
